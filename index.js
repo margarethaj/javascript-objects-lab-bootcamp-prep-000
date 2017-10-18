bcd21c82201a1998f1e3b4597c665bcac81f690f
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-18 01:46:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = new Object({ tes : 'tes1' });
+
+function Objects(){
+  return recipes;
+}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key] : value});
+}
+
+functions destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  //object[key] = value;
+  return object
+}
+
